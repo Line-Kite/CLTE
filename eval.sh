2f2aeecd45119fd3a03cca5c3cbb8d1787756461
Line-Kite <qw-line@whu.edu.cn>
2025-11-15 22:45:43 +0800

    framework

--- eval.sh
+++ eval.sh
@@ -1,5 +1,5 @@
- CUDA_VISIBLE_DEVICES=5,6 python eval.py --model_path /data1/liqw/checkpoints/Qwen3-8B \
+ CUDA_VISIBLE_DEVICES=0 python eval.py --model_path Qwen/Qwen3-8B \
     --data_dir data \
     --save_dir results \
     --max_length 4096 \
-    --test_time 1 
+    --test_time 3 
